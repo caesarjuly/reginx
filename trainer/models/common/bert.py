9c4fd74400a38e59616f79f4f8af0dabe0ec4cbe
fan.yang <julyyangfan@gmail.com>
2023-08-31 15:06:02 -0700

    Bert example

--- trainer/models/common/bert.py
+++ trainer/models/common/bert.py
@@ -17,21 +17,22 @@ class BertEmbedding(tf.keras.layers.Layer):
         - [BERT: Pre-training of Deep Bidirectional Transformers for Language Understanding](https://arxiv.org/pdf/1810.04805.pdf)
     """
 
-    def __init__(self, vocab_size, length=128, dim=512, **kwargs):
+    def __init__(self, vocab_size, seq_length=128, dim=512, **kwargs):
         super(BertEmbedding, self).__init__(**kwargs)
-        assert length % 2 == 0, "Output dimension needs to be an even integer"
-        self.length = length
+        assert seq_length % 2 == 0, "Output dimension needs to be an even integer"
+        self.length = seq_length
         self.dim = dim
         self.token_emb = tf.keras.layers.Embedding(
             input_dim=vocab_size, output_dim=dim, mask_zero=True
         )
-        self.position_emb = tf.keras.layers.Embedding(input_dim=length, output_dim=dim)
-        self.segment_emb = tf.keras.layers.Embedding(
-            input_dim=2, output_dim=dim, mask_zero=True
+        self.position_emb = tf.keras.layers.Embedding(
+            input_dim=seq_length, output_dim=dim
         )
+        self.segment_emb = tf.keras.layers.Embedding(input_dim=2, output_dim=dim)
 
     def call(self, inputs, **kwargs):
-        tokens, segments = inputs
+        tokens = inputs["input_word_ids"]
+        segments = inputs["input_type_ids"]
         length = tf.shape(tokens)[1]
         embedded_tokens = self.token_emb(tokens)
         embedded_segments = self.segment_emb(segments)
@@ -43,8 +44,8 @@ class BertEmbedding(tf.keras.layers.Layer):
         )
 
     # Pass mask from token_emb, https://www.tensorflow.org/guide/keras/understanding_masking_and_padding#supporting_masking_in_your_custom_layers
-    def compute_mask(self, *args, **kwargs):
-        return self.token_emb.compute_mask(*args, **kwargs)
+    def compute_mask(self, inputs, mask=None):
+        return inputs["input_mask"]
 
 
 class Bert(tf.keras.layers.Layer):
@@ -73,7 +74,7 @@ class Bert(tf.keras.layers.Layer):
     ):
         super(Bert, self).__init__(**kwargs)
         self.emb = BertEmbedding(
-            vocab_size=vocab_size, length=seq_length, dim=model_dim
+            vocab_size=vocab_size, seq_length=seq_length, dim=model_dim
         )
         self.encoders = [
             Encoder(
@@ -84,7 +85,7 @@ class Bert(tf.keras.layers.Layer):
 
     def call(self, inputs, training=False):
         # shape [batch_size, token_length]
-        emb = self.dropout(self.emb(inputs, training=training))
+        emb = self.emb(inputs, training=training)
         # shape [batch_size, token_length, model_dim]
         for encoder in self.encoders:
             emb = encoder(emb, training=training)
@@ -116,7 +117,7 @@ class BertMLM(tf.keras.layers.Layer):
         head_num=8,
         **kwargs,
     ):
-        super(Bert, self).__init__(**kwargs)
+        super(BertMLM, self).__init__(**kwargs)
 
         self.emb = Bert(
             vocab_size=vocab_size,
@@ -134,10 +135,12 @@ class BertMLM(tf.keras.layers.Layer):
         )
 
     def call(self, inputs, training=False):
-        # shape [batch_size, token_length]
+        # shape [batch_size, token_length, model_dim]
         emb = self.dropout(self.emb(inputs, training=training))
         # shape [batch_size, token_length, vocab_size_logits]
-        return self.dense(emb)
+        emb = self.dense(emb)
+        # gather the corresponding logits per the masked_lm_positions
+        return tf.gather(emb, inputs["masked_lm_positions"], axis=1, batch_dims=1)
 
 
 class BertNSP(tf.keras.layers.Layer):
@@ -164,7 +167,7 @@ class BertNSP(tf.keras.layers.Layer):
         head_num=8,
         **kwargs,
     ):
-        super(Bert, self).__init__(**kwargs)
+        super(BertNSP, self).__init__(**kwargs)
 
         self.emb = Bert(
             vocab_size=vocab_size,
@@ -180,7 +183,7 @@ class BertNSP(tf.keras.layers.Layer):
         self.dense = tf.keras.layers.Dense(2, activation=tf.keras.activations.softmax)
 
     def call(self, inputs, training=False):
-        # shape [batch_size, token_length]
+        # shape [batch_size, token_length, model_dim]
         emb = self.dropout(self.emb(inputs, training=training))
         # shape [batch_size, 2]
-        return tf.squeeze(self.dense(emb[:, 0, :]), axis=[1])
+        return self.dense(emb[:, 0, :])
